c36f1e167f7bbf1d01086e29e06360d784c5a05f
Yuliia Dziubalo <yulia.dzubalo@gmail.com>
2023-04-04 23:59:06 +0300

    navbar creating

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from '../Header/Header';
+import Navbar from '../Navbar/Navbar';
+
+const App = () => {
+    return (
+        <Navbar />
+    )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+        <a href='/' className='a-logo'>~ Кондитерські вироби ~</a>
+        <ul>
+            <li>
+                <a href='/'>Головна</a>
+            </li>
+            <li>
+                <a href='/'>Головна</a>
+            </li> 
+            <li>
+                <a href='/'>Головна</a>
+            </li> 
+            <li>
+                <a href='/'>Головна</a>
+            </li> 
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
