8e38cc1b1386f12eaabe2582a8a54ed36780d795
Yuliia Dziubalo <yulia.dzubalo@gmail.com>
2023-06-04 23:36:31 +0300

    04.06

--- src/components/Assortment/ACard/ACard.js
+++ src/components/Assortment/ACard/ACard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './ACard.css'
-import chocolate from '../../../images/chocolate.png'
 
 function ACard(props) {
   return (
+    
     <div className='assort-card'>
         <div>
             <img className='assort-card-img' src={props.src}/>
